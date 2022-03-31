372fee7b33f568e0567d8c657b4eaf24644d1d58
fl-wer <101416707+fl-wer@users.noreply.github.com>
2022-03-31 02:46:39 +0100

    Add files via upload

---
+++ Sensei/Main.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace Sensei
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.perQueueLabel = new System.Windows.Forms.Label();
+            this.perQueueNumeric = new System.Windows.Forms.NumericUpDown();
+            this.processNameLabel = new System.Windows.Forms.Label();
+            this.processNameTextBox = new System.Windows.Forms.TextBox();
+            this.liner_zp0nyc = new System.Windows.Forms.Label();
+            this.liner_kH38Cd = new System.Windows.Forms.Label();
+            this.loadQueueToggle1 = new System.Windows.Forms.Button();
+            this.loadQueueToggle2 = new System.Windows.Forms.Button();
+            this.loadQueueToggle3 = new System.Windows.Forms.Button();
+            this.loadQueueToggle4 = new System.Windows.Forms.Button();
+            this.loadQueueToggle5 = new System.Windows.Forms.Button();
+            this.loadQueueToggle6 = new System.Windows.Forms.Button();
+            this.loadQueueToggle7 = new System.Windows.Forms.Button();
+            this.loadQueueToggle8 = new System.Windows.Forms.Button();
+            this.calculateQueues = new System.Windows.Forms.Button();
+            this.quickSupport = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.alwaysOnTopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.killAllSessionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.minimizeAllToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.perQueueNumeric)).BeginInit();
+            this.quickSupport.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // perQueueLabel
+            // 
+            this.perQueueLabel.AutoSize = true;
+            this.perQueueLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.perQueueLabel.ForeColor = System.Drawing.Color.White;
+            this.perQueueLabel.Location = new System.Drawing.Point(12, 11);
+            this.perQueueLabel.Name = "perQueueLabel";
+            this.perQueueLabel.Size = new System.Drawing.Size(82, 21);
+            this.perQueueLabel.TabIndex = 5;
+            this.perQueueLabel.Text = "Per Queue";
+            // 
+            // perQueueNumeric
+            // 
+            this.perQueueNumeric.Location = new System.Drawing.Point(154, 13);
+            this.perQueueNumeric.Maximum = new decimal(new int[] {
+            12,
+            0,
+            0,
+            0});
+            this.perQueueNumeric.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.perQueueNumeric.Name = "perQueueNumeric";
+            this.perQueueNumeric.Size = new System.Drawing.Size(100, 20);
+            this.perQueueNumeric.TabIndex = 4;
+            this.perQueueNumeric.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // processNameLabel
+            // 
+            this.processNameLabel.AutoSize = true;
+            this.processNameLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.processNameLabel.ForeColor = System.Drawing.Color.White;
+            this.processNameLabel.Location = new System.Drawing.Point(12, 39);
+            this.processNameLabel.Name = "processNameLabel";
+            this.processNameLabel.Size = new System.Drawing.Size(109, 21);
+            this.processNameLabel.TabIndex = 6;
+            this.processNameLabel.Text = "Process Name";
+            // 
+            // processNameTextBox
+            // 
+            this.processNameTextBox.Location = new System.Drawing.Point(154, 41);
+            this.processNameTextBox.Name = "processNameTextBox";
+            this.processNameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.processNameTextBox.TabIndex = 7;
+            // 
+            // liner_zp0nyc
+            // 
+            this.liner_zp0nyc.BackColor = System.Drawing.Color.DimGray;
+            this.liner_zp0nyc.Location = new System.Drawing.Point(17, 75);
+            this.liner_zp0nyc.Name = "liner_zp0nyc";
+            this.liner_zp0nyc.Size = new System.Drawing.Size(230, 2);
+            this.liner_zp0nyc.TabIndex = 8;
+            // 
+            // liner_kH38Cd
+            // 
+            this.liner_kH38Cd.BackColor = System.Drawing.Color.DimGray;
+            this.liner_kH38Cd.Location = new System.Drawing.Point(17, 141);
+            this.liner_kH38Cd.Name = "liner_kH38Cd";
+            this.liner_kH38Cd.Size = new System.Drawing.Size(230, 2);
+            this.liner_kH38Cd.TabIndex = 10;
+            // 
+            // loadQueueToggle1
+            // 
+            this.loadQueueToggle1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loadQueueToggle1.Location = new System.Drawing.Point(12, 155);
+            this.loadQueueToggle1.Name = "loadQueueToggle1";
+            this.loadQueueToggle1.Size = new System.Drawing.Size(242, 41);
+            this.loadQueueToggle1.TabIndex = 11;
+            this.loadQueueToggle1.Text = "1";
+            this.loadQueueToggle1.UseVisualStyleBackColor = true;
+            this.loadQueueToggle1.Click += new System.EventHandler(this.loadQueue);
+            // 
+            // loadQueueToggle2
+            // 
+            this.loadQueueToggle2.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loadQueueToggle2.Location = new System.Drawing.Point(12, 202);
+            this.loadQueueToggle2.Name = "loadQueueToggle2";
+            this.loadQueueToggle2.Size = new System.Drawing.Size(242, 41);
+            this.loadQueueToggle2.TabIndex = 12;
+            this.loadQueueToggle2.Text = "2";
+            this.loadQueueToggle2.UseVisualStyleBackColor = true;
+            this.loadQueueToggle2.Click += new System.EventHandler(this.loadQueue);
+            // 
+            // loadQueueToggle3
+            // 
+            this.loadQueueToggle3.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loadQueueToggle3.Location = new System.Drawing.Point(12, 249);
+            this.loadQueueToggle3.Name = "loadQueueToggle3";
+            this.loadQueueToggle3.Size = new System.Drawing.Size(242, 41);
+            this.loadQueueToggle3.TabIndex = 13;
+            this.loadQueueToggle3.Text = "3";
+            this.loadQueueToggle3.UseVisualStyleBackColor = true;
+            this.loadQueueToggle3.Click += new System.EventHandler(this.loadQueue);
+            // 
+            // loadQueueToggle4
+            // 
+            this.loadQueueToggle4.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loadQueueToggle4.Location = new System.Drawing.Point(12, 296);
+            this.loadQueueToggle4.Name = "loadQueueToggle4";
+            this.loadQueueToggle4.Size = new System.Drawing.Size(242, 41);
+            this.loadQueueToggle4.TabIndex = 14;
+            this.loadQueueToggle4.Text = "4";
+            this.loadQueueToggle4.UseVisualStyleBackColor = true;
+            this.loadQueueToggle4.Click += new System.EventHandler(this.loadQueue);
+            // 
+            // loadQueueToggle5
+            // 
+            this.loadQueueToggle5.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loadQueueToggle5.Location = new System.Drawing.Point(12, 343);
+            this.loadQueueToggle5.Name = "loadQueueToggle5";
+            this.loadQueueToggle5.Size = new System.Drawing.Size(242, 41);
+            this.loadQueueToggle5.TabIndex = 15;
+            this.loadQueueToggle5.Text = "5";
+            this.loadQueueToggle5.UseVisualStyleBackColor = true;
+            this.loadQueueToggle5.Click += new System.EventHandler(this.loadQueue);
+            // 
+            // loadQueueToggle6
+            // 
+            this.loadQueueToggle6.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loadQueueToggle6.Location = new System.Drawing.Point(12, 390);
+            this.loadQueueToggle6.Name = "loadQueueToggle6";
+            this.loadQueueToggle6.Size = new System.Drawing.Size(242, 41);
+            this.loadQueueToggle6.TabIndex = 16;
+            this.loadQueueToggle6.Text = "6";
+            this.loadQueueToggle6.UseVisualStyleBackColor = true;
+            this.loadQueueToggle6.Click += new System.EventHandler(this.loadQueue);
+            // 
+            // loadQueueToggle7
+            // 
+            this.loadQueueToggle7.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loadQueueToggle7.Location = new System.Drawing.Point(12, 437);
+            this.loadQueueToggle7.Name = "loadQueueToggle7";
+            this.loadQueueToggle7.Size = new System.Drawing.Size(242, 41);
+            this.loadQueueToggle7.TabIndex = 17;
+            this.loadQueueToggle7.Text = "7";
+            this.loadQueueToggle7.UseVisualStyleBackColor = true;
+            this.loadQueueToggle7.Click += new System.EventHandler(this.loadQueue);
+            // 
+            // loadQueueToggle8
+            // 
+            this.loadQueueToggle8.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loadQueueToggle8.Location = new System.Drawing.Point(12, 484);
+            this.loadQueueToggle8.Name = "loadQueueToggle8";
+            this.loadQueueToggle8.Size = new System.Drawing.Size(242, 41);
+            this.loadQueueToggle8.TabIndex = 18;
+            this.loadQueueToggle8.Text = "8";
+            this.loadQueueToggle8.UseVisualStyleBackColor = true;
+            this.loadQueueToggle8.Click += new System.EventHandler(this.loadQueue);
+            // 
+            // calculateQueues
+            // 
+            this.calculateQueues.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calculateQueues.Location = new System.Drawing.Point(12, 88);
+            this.calculateQueues.Name = "calculateQueues";
+            this.calculateQueues.Size = new System.Drawing.Size(242, 41);
+            this.calculateQueues.TabIndex = 19;
+            this.calculateQueues.Text = "Calculate Queues";
+            this.calculateQueues.UseVisualStyleBackColor = true;
+            this.calculateQueues.Click += new System.EventHandler(this.calculateQueues_Click);
+            // 
+            // quickSupport
+            // 
+            this.quickSupport.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.alwaysOnTopToolStripMenuItem,
+            this.killAllSessionsToolStripMenuItem,
+            this.minimizeAllToolStripMenuItem});
+            this.quickSupport.Name = "quickSupport";
+            this.quickSupport.Size = new System.Drawing.Size(155, 70);
+            // 
+            // alwaysOnTopToolStripMenuItem
+            // 
+            this.alwaysOnTopToolStripMenuItem.Checked = true;
+            this.alwaysOnTopToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.alwaysOnTopToolStripMenuItem.Name = "alwaysOnTopToolStripMenuItem";
+            this.alwaysOnTopToolStripMenuItem.Size = new System.Drawing.Size(154, 22);
+            this.alwaysOnTopToolStripMenuItem.Text = "Always On Top";
+            this.alwaysOnTopToolStripMenuItem.Click += new System.EventHandler(this.alwaysOnTopToolStripMenuItem_Click);
+            // 
+            // killAllSessionsToolStripMenuItem
+            // 
+            this.killAllSessionsToolStripMenuItem.Name = "killAllSessionsToolStripMenuItem";
+            this.killAllSessionsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.killAllSessionsToolStripMenuItem.Text = "Kill All Sessions";
+            this.killAllSessionsToolStripMenuItem.Click += new System.EventHandler(this.killAllSessionsToolStripMenuItem_Click);
+            // 
+            // minimizeAllToolStripMenuItem
+            // 
+            this.minimizeAllToolStripMenuItem.Name = "minimizeAllToolStripMenuItem";
+            this.minimizeAllToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.minimizeAllToolStripMenuItem.Text = "Minimize All";
+            this.minimizeAllToolStripMenuItem.Click += new System.EventHandler(this.minimizeAllToolStripMenuItem_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.ClientSize = new System.Drawing.Size(266, 139);
+            this.ContextMenuStrip = this.quickSupport;
+            this.Controls.Add(this.calculateQueues);
+            this.Controls.Add(this.loadQueueToggle8);
+            this.Controls.Add(this.loadQueueToggle7);
+            this.Controls.Add(this.loadQueueToggle6);
+            this.Controls.Add(this.loadQueueToggle5);
+            this.Controls.Add(this.loadQueueToggle4);
+            this.Controls.Add(this.loadQueueToggle3);
+            this.Controls.Add(this.loadQueueToggle2);
+            this.Controls.Add(this.loadQueueToggle1);
+            this.Controls.Add(this.liner_kH38Cd);
+            this.Controls.Add(this.liner_zp0nyc);
+            this.Controls.Add(this.processNameTextBox);
+            this.Controls.Add(this.processNameLabel);
+            this.Controls.Add(this.perQueueLabel);
+            this.Controls.Add(this.perQueueNumeric);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.Text = "Sensei";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.perQueueNumeric)).EndInit();
+            this.quickSupport.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label perQueueLabel;
+        private System.Windows.Forms.NumericUpDown perQueueNumeric;
+        private System.Windows.Forms.Label processNameLabel;
+        private System.Windows.Forms.TextBox processNameTextBox;
+        private System.Windows.Forms.Label liner_zp0nyc;
+        private System.Windows.Forms.Label liner_kH38Cd;
+        private System.Windows.Forms.Button loadQueueToggle1;
+        private System.Windows.Forms.Button loadQueueToggle2;
+        private System.Windows.Forms.Button loadQueueToggle3;
+        private System.Windows.Forms.Button loadQueueToggle4;
+        private System.Windows.Forms.Button loadQueueToggle5;
+        private System.Windows.Forms.Button loadQueueToggle6;
+        private System.Windows.Forms.Button loadQueueToggle7;
+        private System.Windows.Forms.Button loadQueueToggle8;
+        private System.Windows.Forms.Button calculateQueues;
+        private System.Windows.Forms.ContextMenuStrip quickSupport;
+        private System.Windows.Forms.ToolStripMenuItem alwaysOnTopToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem killAllSessionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem minimizeAllToolStripMenuItem;
+    }
+}
+
---
+++ Sensei/Main.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sensei
+{
+    public partial class Main : Form
+    {
+        // this is just initializing, ignore
+        public Main() { InitializeComponent(); }
+
+        // build version, used for display
+        public static string softwareVersion = "1";
+
+        // ### imported windows functions
+        // this can minimize or show window, used for loading queues
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        // used for ShowWindow function as second argument
+        const int SW_SHOWMINIMIZED = 2;
+        const int SW_SHOWNORMAL = 1;
+
+        // default window size, used to reset window easier
+        Size defaultWindowSize = new Size(286, 182);
+
+        // global process list, it's global because some functions need
+        // this and they are externall callers that I can't pass arguments to
+        Process[] processList;
+
+        // path where all INF files are stored
+        string configFilesPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) +
+            "\\fl-wer\\Sensei\\";
+
+        // information file names, these are files that store all
+        // kind of information and are saved in software default folder in variable below
+        // all those files will have "INF" at the end so you know these
+        public static string alwaysOnTopINF = "p44gz4";
+        public static string lastProcessNameINF = "5gf1mv";
+
+        // running on startup
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // disable north korean thread control
+            CheckForIllegalCrossThreadCalls = false;
+
+            // make sure config files (INF) folder exists, if not make it
+            if (!Directory.Exists(configFilesPath))
+                Directory.CreateDirectory(configFilesPath);
+
+            // load all saved settings
+            loadSettings();
+        }
+
+        // adjust window height based on calculated queue quantity
+        private void calculateQueues_Click(object sender, EventArgs e)
+        {
+            // get all processes
+            processList = Process.GetProcessesByName(processNameTextBox.Text);
+
+            // calculate queue quantity
+            int queueQuantity = getQueueQuantity();
+
+            // do some process validation checks
+            if (processList.Length == 0) ShowError("Process not running.");
+            else if (processList.Length < 2) ShowError("At least two process instances required.");
+            else if (queueQuantity < 1) ShowError("At least 1 queue required, change your settings.");
+            else if (queueQuantity > 8) ShowError("Queue limit of 8 reached, change your settings.");
+
+            // validation passed, adjusting windows height
+            else Size = calculateWindowSize(queueQuantity);
+
+            // save last used process to INF (settings)
+            File.WriteAllText(configFilesPath + lastProcessNameINF, processNameTextBox.Text);
+        }
+
+        // hide all windows and show only selected queue
+        void loadQueue(object sender, EventArgs e)
+        {
+            // getting button object that was used to invoke this
+            Button clickedButton = sender as Button;
+
+            // parsing button text for shortening code, they all have number as text
+            // with this we don't have to use 8 ifs
+            int parseQueueNumber = Convert.ToInt32(clickedButton.Text);
+
+            // minimize all windows, clear the view before doing anything
+            foreach (Process proc in processList)
+                ShowWindow(proc.MainWindowHandle, SW_SHOWMINIMIZED);
+
+            // converting numeric value from control for performance / readability
+            int perQueue = (int)perQueueNumeric.Value;
+
+            // we start from the queue we clicked on, queue number * items per queue
+            for (int i = perQueue * (parseQueueNumber - 1); i < perQueue * parseQueueNumber; i++)
+                // making sure we're not exceeding process array, eg would be someone
+                // closing the process and then trying to load the queue, break if so
+                if (i < processList.Length) ShowWindow(processList[i].MainWindowHandle, SW_SHOWNORMAL);
+                else break;
+        }
+
+        // this returns windows size with changed height to reveal hidden
+        // queue buttons, based on how many process and per queue are set
+        Size calculateWindowSize(int queueQuantity)
+        {
+            // ### calculate window size
+            // if one queue only we can then used preset size
+            if (queueQuantity == 1) return new Size(286, 246); // 294 = 2 queues
+
+            // if more than 1 queue we need to calculate, 47 is based on controls location
+            // which was set when I was designing the form
+            else if (queueQuantity > 1) return new Size(286, 246 + (queueQuantity - 1) * 47);
+
+            // if queue quantity == 0 we're returning default window size
+            else return defaultWindowSize;
+        }
+
+        // calculated amount of queues
+        int getQueueQuantity()
+        {
+            // diving per queue 
+            int queueQuantity = (processList.Length / (int)perQueueNumeric.Value);
+
+            // if there are more process it means that the number wasn't integer
+            // and also wasn't rounded up, eg processes = 7, per queue = 3, meaning
+            // there will be 1 process left (7/2 = 6, 7-6 = 1) so 3 queues are required, not 2
+            if (queueQuantity * (int)perQueueNumeric.Value < processList.Length) queueQuantity += 1;
+
+            // return final result
+            return queueQuantity;
+        }
+
+        // load saved configuration files (options and settings)
+        void loadSettings()
+        {
+            // always on top
+            if (File.Exists(configFilesPath + alwaysOnTopINF))
+            {
+                // not doing anything if True because True is default
+                if (File.ReadAllText(configFilesPath + alwaysOnTopINF) == "False")
+                {
+                    // changing window property
+                    TopMost = false;
+
+                    // adjusting checkbox on context menu strip item for Always On Top
+                    ((ToolStripMenuItem)quickSupport.Items[0]).Checked = false;
+                }
+            }
+
+            // last process name
+            if (File.Exists(configFilesPath + lastProcessNameINF))
+                processNameTextBox.Text = File.ReadAllText(configFilesPath + lastProcessNameINF);
+        }
+
+        // make the window always visiblle, won't minimize if clicked somewhere else
+        private void alwaysOnTopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // disable / enable TopMost property of the window
+            TopMost = !TopMost;
+
+            // based on if TopMost is enabled or not change checkbox accordingly
+            ((ToolStripMenuItem)quickSupport.Items[0]).Checked = TopMost;
+
+            // save button to INF (settings), True or False (in text)
+            File.WriteAllText(configFilesPath + alwaysOnTopINF, TopMost.ToString());
+        }
+
+        // kill all instances of the process
+        private void killAllSessionsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // go through list of all processess and use Kill function to kill all
+            foreach (Process proc in Process.GetProcessesByName(processNameTextBox.Text))
+                proc.Kill();
+        }
+
+        // minimize all windows for the process
+        private void minimizeAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // go through list of all processess and use ShowWindow function to minimize all
+            foreach (Process proc in Process.GetProcessesByName(processNameTextBox.Text))
+                ShowWindow(proc.MainWindowHandle, SW_SHOWMINIMIZED);
+        }
+
+        // pre-created message boxes for showing errors or info
+        void ShowError(string message) { MessageBox.Show(message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error); }
+        void ShowInfo(string message) { MessageBox.Show(message, "Info", MessageBoxButtons.OK, MessageBoxIcon.Information); }
+    }
+}
